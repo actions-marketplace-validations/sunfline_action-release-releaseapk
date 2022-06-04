ef59be34f68f270a1262e312ab28408f6670b0f8
evonicgu <57173883+evonicgu@users.noreply.github.com>
2022-06-04 08:41:36 +0300

    Search for version inside top-level gradle file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 hub checkout ${${REPO_BRANCH}:-master}
-VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
+VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./build.gradle`
 hub release create -a ${PATH_TO_APK} -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
